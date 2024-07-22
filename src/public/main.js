2721fae5ecb295b0b4e9e52f8ecbe7e1a73ff13b
Patrick <patrick.zhang0502@gmail.com>
2024-07-22 00:15:54 -0400

    fixed the 3D graphic

--- src/public/main.js
+++ src/public/main.js
@@ -1,26 +1,39 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+const container = document.getElementById('top-graphic')
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( container.offsetWidth, container.offsetHeight);
 renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+container.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 2, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshMatcapMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 6;
-camera.position.x = 1;
+camera.position.z = 5;
 
 function animate() {
-
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-
+	cube.rotation.x += 0.005;
+	cube.rotation.y += 0.005;
 	renderer.render( scene, camera );
 
 }
+
+// Resize
+
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+const loop = () => {
+	animate()
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(loop)
+}
+loop()
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#0066CC',
+      }
+    },
   },
   plugins: [],
 }
