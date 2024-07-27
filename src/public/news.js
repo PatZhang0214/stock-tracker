3cc06769bb7129bcd65876ba722ff31b58015225
Patrick <patrick.zhang0502@gmail.com>
2024-07-27 17:10:23 -0400

    added needed dependencies

---
+++ src/public/news.js
@@ -0,0 +1,7 @@
+var newsAPI = require('@apiverve/news');
+const test = require('dotenv').config({path: './public/.env'})
+console.log(test)
+// var api = new newsAPI({
+//     api_key: [],
+//     secure: true //(Optional, defaults to true)
+// });
